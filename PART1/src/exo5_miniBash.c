93e9818c0e38c73c46be8777757a509cc2fca4f5
SyphaxMEDJBER <syphax.medjber.8@gmail.com>
2025-11-29 15:19:36 +0100

    miniBah testé: OK

--- PART1/src/exo5_miniBash.c
+++ PART1/src/exo5_miniBash.c
@@ -30,7 +30,7 @@ void miniBash(){
     for(int i=0;argv[i]==NULL;i++){
       free(argv[i]);
     }
-    free(argv)
+    free(argv);
 
 
   }
---
+++ PART1/test/test5.c
@@ -0,0 +1,7 @@
+#include "../include/tp1.h"
+
+int main(){
+  miniBash();
+
+  return 0;
+}
