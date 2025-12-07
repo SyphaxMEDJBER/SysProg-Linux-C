d3bbf70773a19beb571a15e53667a552f727a074
SyphaxMEDJBER <syphax.medjber.8@gmail.com>
2025-12-07 17:19:41 +0100

    la structure commande est ajouté

--- PART1/include/tp1.h
+++ PART1/include/tp1.h
@@ -34,4 +34,32 @@ int executerBatch(char **);
 
 void execFileBatch(char* file);
 
+
+// Epoch : la gestion du temps en informatique se fait généralement à l'aide d'une valeur (nommée 
+// Epoch). Elle représente le nombre de secondes écoulées depuis le 1er janvier 1970. Son type de time_t
+// sous UNIX mais c'est en fait un entier.
+// Pour obtenir le temps actuel il faut utiliser la fonction time(time_t *) qui retourne l'Epoch de moment 
+// de l'appel. Elle est déclarée dans le fichier time.h. 
+// #include <stdio.h>
+// #include <time.h>
+// int main()
+// {
+// time_t now ;
+// time(&now) ;
+// printf(" l\'epoch actuel est : %d\n",now) ;
+// }
+
+
+
+typedef struct commande{
+  int pid;//numero de processus dans lequel sexecute la commande 
+  int status;//son statut : -1 pas encore executé , 0 terminé , 1 en execution
+  int retour;//son retour EXITSTATUS
+  time_t debut;//'epoch à laquelle la commande a été lancée
+  time_t fin;//'epoch à laquelle la commande s'est terminée
+  char **argv;//son tableau argv 
+
+
+}cmd;
+
 #endif
